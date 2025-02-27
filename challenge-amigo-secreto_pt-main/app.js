f3664d3b35484aa3469a0d9b5236bd85bdec7fa1
Julia Souza <julia.souza18@eaportal.org>
2025-02-27 11:33:15 -0300

    botoes funcionando, achar erro de adicionar o nome

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,32 +1,37 @@
-let  listaDeAmigos =[];
+let amigos = [];
+
 function adicionarAmigo() {
-   
-    const nomeAmigo = document.getElementById("nomeAmigo").value;
-  
-    
-    if (nomeAmigo.trim() === "") {
-     
-      alert("Por favor, insira um nome.");
-      return;
-    }
-  
-    
-    listaDeAmigos.push(nomeAmigo);
-  
-    
-    document.getElementById("nomeAmigo").value = "";
-  
-    
-    atualizarListaDeAmigos();
-  }
-  
-  function atualizarListaDeAmigos() {
-    const listaAmigosElement = document.getElementById("listaAmigos");
-    listaAmigosElement.innerHTML = ""; 
-  
-    listaDeAmigos.forEach((amigo) => {
-      const itemLista = document.createElement("li");
-      itemLista.textContent = amigo;
-      listaAmigosElement.appendChild(itemLista);
-    });
-  }
+    console.log('botao okay');
+}
+
+if (adicionarAmigo() === "") {
+  alert("Por favor, insira um nome.");
+}
+
+amigos.push(amigos);{
+document.getElementById("nomeAmigo"); 
+atualizarListaAmigos();
+}
+
+function atualizarListaAmigos() {
+const lista = document.getElementById("listaAmigos");
+lista.innerHTML = "";
+
+for (let amigo of amigos) {
+  const itemLista = document.createElement("li");
+  itemLista.textContent = amigo;
+  lista.appendChild(itemLista);
+}
+}
+
+function sortearAmigo() {
+  console.log('botao sim');
+if (adicionarAmigo.length === 0) {
+  alert("Adicione amigos antes de sortear.");
+  return;
+}
+
+const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+const amigoSorteado = amigos[indiceAleatorio];
+document.getElementById("amigoSorteado").textContent; 
+}
