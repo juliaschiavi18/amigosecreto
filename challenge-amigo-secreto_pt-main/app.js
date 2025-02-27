07a0e02c2e17dfedd523ff8d9aacccb5f7f73ccf
Julia Souza <julia.souza18@eaportal.org>
2025-02-27 10:38:19 -0300

    primeiro topico

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.'
+let  listaDeAmigos =[];
+function adicionarAmigo() {
+   
+    const nomeAmigo = document.getElementById("nomeAmigo").value;
+  
+    
+    if (nomeAmigo.trim() === "") {
+     
+      alert("Por favor, insira um nome.");
+      return;
+    }
+  
+    
+    listaDeAmigos.push(nomeAmigo);
+  
+    
+    document.getElementById("nomeAmigo").value = "";
+  
+    
+    atualizarListaDeAmigos();
+  }
+  
+  function atualizarListaDeAmigos() {
+    const listaAmigosElement = document.getElementById("listaAmigos");
+    listaAmigosElement.innerHTML = ""; 
+  
+    listaDeAmigos.forEach((amigo) => {
+      const itemLista = document.createElement("li");
+      itemLista.textContent = amigo;
+      listaAmigosElement.appendChild(itemLista);
+    });
+  }
